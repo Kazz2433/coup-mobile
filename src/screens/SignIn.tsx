a5995b67c9e28aaf3def0c4da3a888d65be27c59
Kazz2433 <kelvinquida24@gmail.com>
2022-11-16 09:28:56 -0400

    image added to SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,13 @@
-import {Center,Text} from 'native-base'
+import React from 'react';
+import {Center,Text,Image} from 'native-base';
+import Logo from '../assets/logo.svg'
 
 export function SignIn(){
     return (
           <Center flex={1} bgColor='gray.900'>
-            <Text color='white' fontSize={24} fontFamily='heading'>
-                SignIn
-            </Text>
+            <Image source={{
+              uri:"https://toppng.com/uploads/preview/trophy-11527593161wjmswjufrj.png"
+              }}alt="Alternate Text" size="xl" />
           </Center>
       );  
 }
