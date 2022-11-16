3bbb8004c6d579dd56addff885ed569079dba814
Kazz2433 <kelvinquida24@gmail.com>
2022-11-16 10:40:50 -0400

    SignIn screen completed

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {Center,Text,Image} from 'native-base';
-import Logo from '../assets/logo.svg'
+import {Center,Text,Image, Icon} from 'native-base';
+import {Fontisto} from '@expo/vector-icons'
+import { Button } from '../components/Button';
 
 export function SignIn(){
     return (
-          <Center flex={1} bgColor='gray.900'>
+          <Center flex={1} bgColor='gray.900' p={7}>
             <Image source={{
               uri:"https://toppng.com/uploads/preview/trophy-11527593161wjmswjufrj.png"
               }}alt="Alternate Text" size="xl" />
+
+              <Button 
+                type='SECONDARY'
+                title='ENTRAR COM O GOOGLE'
+                leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+                mt={12}
+              />
+
+              <Text color='white' textAlign='center' mt={4}>
+                Nao utilizamos nenhuma informação além {'\n'} do seu e-mail para criação da conta
+              </Text>
           </Center>
       );  
 }
