a07fbc9875a3586d14777e0042c62e1ba8a445a3
Kazz2433 <kelvinquida24@gmail.com>
2022-11-17 22:08:31 -0400

    api context on SignIn page

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import {Center,Text,Image, Icon} from 'native-base';
 import {Fontisto} from '@expo/vector-icons'
 import { Button } from '../components/Button';
+import {useAuth} from '../hooks/useAuth'
 
 export function SignIn(){
+    const {signIn, user} = useAuth()
+
+    console.log("DADOS DO USER",user)
+
     return (
           <Center flex={1} bgColor='gray.900' p={7}>
             <Image source={{
@@ -15,6 +20,7 @@ export function SignIn(){
                 title='ENTRAR COM O GOOGLE'
                 leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
                 mt={12}
+                onPress={signIn}
               />
 
               <Text color='white' textAlign='center' mt={4}>
