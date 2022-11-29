37c36b713c35b6b6fd1ef6bf788f9e473ed095d5
Kazz2433 <kelvinquida24@gmail.com>
2022-11-28 23:20:05 -0400

    added route and bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import {NativeBaseProvider, Text,Center,StatusBar} from 'native-base'
 import {useFonts,Roboto_400Regular,Roboto_500Medium,Roboto_700Bold} from '@expo-google-fonts/roboto'
 
-import { SignIn } from './src/screens/SignIn';
-import {Loading} from './src/components/Loading'
 
 import { THEME } from './src/styles/theme';
+import { Routes } from './src/routes';
+import {Loading} from './src/components/Loading'
 import {AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
           translucent
         />
           
-          {fontsLoaded ? <SignIn /> : <Loading />}
+          {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
